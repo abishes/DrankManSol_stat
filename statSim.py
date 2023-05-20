f30b05391cdce4c20a5fb45c4f4bc43392cdd051
abishes <84623606+abishes@users.noreply.github.com>
2023-05-20 11:27:04 +0545

    simple fix

--- statSim.py
+++ statSim.py
@@ -3,14 +3,13 @@ import numpy as np
 from matplotlib import pyplot as plt
 N = 1000
 displacement=[]
-for i in range(0,N):
+for j in range(0,N):
     thita = np.random.randint(361, size=N)
     x = np.cos(thita)
     y = np.sin(thita)
     for i in range(0, N):
-        if i!=0:
-            x[i] = x[i-1] + x[i]
-            y[i] = y[i-1] + y[i]
+        x[i] = x[i-1] + x[i]
+        y[i] = y[i-1] + y[i]
     displacement.append(sqrt(x[N-1]*x[N-1]+y[N-1]*y[N-1]))
 print(f"Max distance is {max(displacement)} and average distance is {np.average(displacement)}")
 plt.plot(x,y)
